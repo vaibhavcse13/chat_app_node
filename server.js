52aaa0c63623a714f1c5b26c1de278ef67f0e45f
Vaibhav Shukla <cse.vaibhavshukla@gmail.com>
2018-08-20 23:32:15 -0700

    nested callback

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ app.get('/messages' , (req , res) => {
     Message.find({} , (err , messages) =>{
         if(err)
             res.sendStatus(500);
+            
         res.send(messages)
+
     })
 });
 
@@ -29,12 +31,25 @@ app.get('/messages' , (req , res) => {
 app.post('/message' , (req , res) => {
     
     var message =  new Message(req.body);
-    message.save((err)=>{
+    message.save((err) => {
         if(err)
             res.sendStatus(500);
 
-            io.emit('message' , req.body);
-            res.sendStatus(200)
+        Message.findOne( {'message' : 'fuck'} , (err , censored) => {
+
+            console.log(censored)
+            if(err)
+                console.log("Error occured while filtering" ,  err)
+
+            if(censored){
+                console.log("censered word found" , censored);
+                Message.remove({_id : censored.id } , (err) => {
+                    console.log("word removed")
+                })
+            } 
+        })
+        io.emit('message' , req.body);
+        res.sendStatus(200)
     })
    
 });
@@ -43,7 +58,7 @@ io.on('connection' , (socket) => {
     console.log("user connected");
 });
 
-mongoose.connect(dbUrl, (err)=>{
+mongoose.connect(dbUrl, {useNewUrlParser:true}, (err)=>{
     console.log("database connected " , err);
 })
 var server =  http.listen(3000 ,  ()=>{
